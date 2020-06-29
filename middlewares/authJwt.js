d4f2852fb4d717150f07215c2811e9ac6dabecb5
Iamabayomi <Oladinifolawiyo@gmail.com>
2020-06-29 23:30:57 +0100

    added method to determine moderator or Admin

--- middlewares/authJwt.js
+++ middlewares/authJwt.js
@@ -55,3 +55,36 @@ isModerator = (req, res, next) => {
         });
     });
 }; 
+
+
+isModeratorOrAdmin = (req, res, next) => {
+    User.findByPk(req.userId).then(user => {
+        user.getRoles().then(roles => {
+            for(let i =0; i< roles.length; i++){
+                if(roles[i.name === "moderator"]){
+                    next();
+                    return;
+                }
+                
+                if(roles[i].name === "admin"){
+                    next();
+                    return;
+                }
+            }
+
+            res.status(403).send({
+                message: "Require Moderator or Admin Role!"
+            });
+        });
+    });
+}
+
+
+const authJwt = {
+    verifyToken : verifyToken,
+    isAdmin : isAdmin,
+    isModerator: isModerator,
+    isModeratorOrAdmin: isModeratorOrAdmin
+};
+
+module.exports = authJwt
